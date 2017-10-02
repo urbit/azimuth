76de06d72a971f49c305c1156255403e6c092296
Fang <fang@fang.io>
2017-10-03 00:24:33 +0200

    Verbose comments in the Votes contract's code.

--- contracts/Votes.sol
+++ contracts/Votes.sol
@@ -10,14 +10,27 @@ contract Votes is Ownable
   event ConcreteMajority(address constitution);
   event AbstractMajority(bytes32 hash);
 
+  // we depend on the constitution to tell us how many voters there are, so that
+  // we can determine how many votes are necessary for a majority.
   uint8 public totalVoters;
 
+  // for concrete votes, we keep track of them *per constitution*, so that votes
+  // from a "previous era" can't take effect when an upgrade has already
+  // happened. this prevents issues with incompatible constitutions.
+
   // per constitution, we keep track of the votes for each proposed address.
+  // we use these to determine whether a vote gets added or retracted.
   mapping(address => mapping(address => bool[256])) private concreteVotes;
+  // we also keep track of vote counts per proposed address.
+  // we use these to determine majorities.
   mapping(address => mapping(address => uint8)) public concreteVoteCounts;
 
+  // for abstract votes, we do the same:
+  // keep track of individual votes,
   mapping(bytes32 => bool[256]) private abstractVotes;
+  // and total counts.
   mapping(bytes32 => uint8) public abstractVoteCounts;
+  // we store an append-only list of proposals that have achieved majority.
   bytes32[] public abstractMajorities;
 
   function Votes()
@@ -25,12 +38,17 @@ contract Votes is Ownable
     //
   }
 
+  // to be called by the constitution whenever appropriate.
+  // (whenever a galaxy becomes living.)
   function incrementTotalVoters()
     external
     onlyOwner
   {
     totalVoters = totalVoters + 1;
   }
+
+  // we provide these getter functions so that clients can easily check if their
+  // would-be vote actually differs from their currently registered one.
 
   function getVote(uint8 _galaxy, address _proposal)
     external
@@ -65,15 +83,23 @@ contract Votes is Ownable
     onlyOwner
     returns(bool newMajority)
   {
+    // can't vote for the currently active constitution.
     require(_proposal != owner);
     bool prev = concreteVotes[owner][_proposal][_galaxy];
+    // vote must differ from what is already registered, to discourage
+    // unnecessary work.
     require(prev != _vote);
+    // update the vote.
     concreteVotes[owner][_proposal][_galaxy] = _vote;
     uint8 oldCount = concreteVoteCounts[owner][_proposal];
+    // when voting yes,
     if (_vote)
     {
+      // increment the proposal's vote count.
       uint8 newCount = oldCount + 1;
       concreteVoteCounts[owner][_proposal] = newCount;
+      // if that makes it a majority vote, return true to notify the
+      // constitution.
       if (newCount > totalVoters / 2)
       {
         ConcreteMajority(_proposal);
@@ -81,6 +107,7 @@ contract Votes is Ownable
       } else {
         return false;
       }
+    // when voting no, simply decrement the proposal's vote count.
     } else {
       concreteVoteCounts[owner][_proposal] = oldCount - 1;
     }
@@ -96,18 +123,23 @@ contract Votes is Ownable
     onlyOwner
   {
     bool prev = abstractVotes[_proposal][_galaxy];
+    // vote must differ from what is already registered, to discourage
+    // unnecessary work.
     require(prev != _vote);
     abstractVotes[_proposal][_galaxy] = _vote;
+    // when voting yes,
     if (_vote)
     {
       uint8 oldCount = abstractVoteCounts[_proposal];
       bool wasMajority = oldCount > totalVoters / 2;
       abstractVoteCounts[_proposal] = oldCount + 1;
+      // if the proposal just became a majority, add it to the list.
       if (!wasMajority && oldCount + 1 > totalVoters / 2)
       {
         abstractMajorities.push(_proposal);
         AbstractMajority(_proposal);
       }
+    // when voting no, simply decrement the proposal's vote count.
     } else {
       abstractVoteCounts[_proposal] = abstractVoteCounts[_proposal] - 1;
     }
