47c8d6530856a6e7fd1c3c7df9bee7e4e552912c
Fang <fang@fang.io>
2018-03-26 23:47:46 +0200

    Make old Constitution call the new one on-upgrade.

--- contracts/ConstitutionBase.sol
+++ contracts/ConstitutionBase.sol
@@ -15,25 +15,34 @@ contract ConstitutionBase is Ownable
   Ships public ships; // ships data storage
   Votes public votes; // votes data storage
 
-  function ConstitutionBase()
+  address public previousConstitution;
+
+  function ConstitutionBase(address _previous)
     internal
   {
-    //
+    previousConstitution = _previous;
+  }
+
+  //  upgraded(): called by previous constitution when upgrading
+  //
+  function upgraded()
+    external
+  {
+    require(msg.sender == previousConstitution);
   }
 
   //  upgrade(): transfer ownership of the constitution data to the new
-  //             constitution contract, then self-destruct. 
+  //             constitution contract, notify it, then self-destruct.
   //
-  //    Note: any eth that have somehow ended up in the contract are also 
+  //    Note: any eth that have somehow ended up in the contract are also
   //          sent to the new constitution.
   //
-  //    XX: old constitution should call an update hook function on the new.
-  //
-  function upgrade(address _new)
+  function upgrade(ConstitutionBase _new)
     internal
   {
     ships.transferOwnership(_new);
     votes.transferOwnership(_new);
+    _new.upgraded();
     Upgraded(_new);
     selfdestruct(_new);
   }
