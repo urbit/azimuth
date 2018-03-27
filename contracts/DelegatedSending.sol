1542204844c972d937fb844b4a3af8d0f6619d5c
Fang <fang@fang.io>
2018-03-27 16:23:43 +0200

    Rename PlanetInvites to DelegatedSending, clean up code and write tests.

---
+++ contracts/DelegatedSending.sol
@@ -0,0 +1,110 @@
+// simple planet invitation management contract
+// draft
+
+pragma solidity 0.4.18;
+
+import './Constitution.sol';
+
+contract DelegatedSending
+{
+  //  Sent: :by sent :ship.
+  //
+  event Sent(uint32 by, uint32 ship);
+
+  //  ships: the ships contract
+  //
+  Ships public ships;
+
+  //  limits: per star, the maximum amount of planets any of its planets may
+  //          give away
+  //
+  mapping(uint16 => uint16) public limits;
+
+  //  sent: per planet, the amount of planets they have sent
+  //
+  mapping(uint32 => uint16) public sent;
+
+  //  DelegatedSending(): register the ships contract
+  //
+  function DelegatedSending(Ships _ships)
+    public
+  {
+    ships = _ships;
+  }
+
+  //  configureLimit(): as the owner of a star, configure the amount of
+  //                    planets that may be given away per ship.
+  //
+  function configureLimit(uint16 _prefix, uint16 _limit)
+    external
+    shipOwner(_prefix)
+  {
+    limits[_prefix] = _limit;
+  }
+
+  //  sendShip(): as the ship _as, spawn the ship _ship to _to.
+  //
+  function sendShip(uint32 _as, uint32 _ship, address _to)
+    external
+    shipOwner(_as)
+  {
+    require(canSend(_as, _ship));
+
+    //  caller may not send to themselves
+    //
+    require(msg.sender != _to);
+
+    //  increment the sent counter for _as.
+    //
+    sent[_as] = sent[_as] + 1;
+
+    //  grant _to ownership of _ship.
+    //
+    Constitution(ships.owner()).spawn(_ship, _to);
+
+    Sent(_as, _ship);
+  }
+
+  //  canSend(): check whether current conditions allow _as to send _ship
+  //
+  function canSend(uint32 _as, uint32 _ship)
+    public
+    view
+    returns (bool result)
+  {
+    uint16 prefix = ships.getPrefix(_as);
+    return ( //  can only send ships with the same prefix
+             //
+             (prefix == ships.getPrefix(_ship)) &&
+             //
+             //  _as must not have hit the allowed limit yet
+             //
+             (sent[_as] < limits[prefix]) &&
+             //
+             //  _ship needs to be inactive
+             //
+             !ships.isActive(_ship) &&
+             //
+             //  this contract must have permission to spawn ships
+             //
+             ships.isSpawnProxy(prefix, this) &&
+             //
+             //  the prefix must not have hit its spawn limit yet
+             //
+             ( ships.getSpawnCount(prefix) <
+               Constitution(ships.owner())
+               .getSpawnLimit(prefix, block.timestamp) ) &&
+             //
+             //  the prefix must be live
+             //
+             (0 != ships.getKeyRevisionNumber(prefix)) );
+  }
+
+  //  shipOwner(): require that :msg.sender is the owner of _ship
+  //
+  modifier shipOwner(uint32 _ship)
+  {
+    require(ships.isOwner(_ship, msg.sender));
+    _;
+  }
+}
---
+++ test/TestDelegatedSending.js
@@ -0,0 +1,70 @@
+const Ships = artifacts.require('../contracts/Ships.sol');
+const Votes = artifacts.require('../contracts/Votes.sol');
+const Constitution = artifacts.require('../contracts/Constitution.sol');
+const DelegatedSending = artifacts.require('../contracts/DelegatedSending.sol');
+
+contract('DelegatedSending', function([owner, user]) {
+  let ships, constit, dese;
+
+  function assertJump(error) {
+    assert.isAbove(error.message.search('revert'), -1, 'Revert must be returned, but got ' + error);
+  }
+
+  before('setting up for tests', async function() {
+    ships = await Ships.new();
+    votes = await Votes.new();
+    constit = await Constitution.new(ships.address, votes.address);
+    await ships.transferOwnership(constit.address);
+    dese = await DelegatedSending.new(ships.address);
+    //
+    await constit.createGalaxy(0, owner);
+    await constit.configureKeys(0, 0, 0);
+    await constit.spawn(256, owner);
+    await constit.configureKeys(256, 0, 0);
+    await constit.spawn(65792, owner);
+    await constit.setSpawnProxy(256, dese.address);
+  });
+
+  it('configuring', async function() {
+    assert.equal(await dese.limits(256), 0);
+    assert.isFalse(await dese.canSend(65792, 131328));
+    // can only be done by star owner.
+    try {
+      await dese.configureLimit(256, 1, {from:user});
+      assert.fail('should have thrown before');
+    } catch(err) {
+      assertJump(err);
+    }
+    await dese.configureLimit(256, 1);
+    assert.equal(await dese.limits(256), 1);
+    assert.isTrue(await dese.canSend(65792, 131328));
+  });
+
+  it('sending', async function() {
+    // can only be done by ship owner
+    try {
+      await dese.sendShip(65792, 131328, user, {from:user});
+      assert.fail('should have thrown before');
+    } catch(err) {
+      assertJump(err);
+    }
+    // can't send to self
+    try {
+      await dese.sendShip(65792, 131328, owner);
+      assert.fail('should have thrown before');
+    } catch(err) {
+      assertJump(err);
+    }
+    await dese.sendShip(65792, 131328, user);
+    assert.isTrue(await ships.isOwner(131328, user));
+    assert.isFalse(await dese.canSend(65792, 131328));
+    // can't send more than the limit.
+    assert.isFalse(await dese.canSend(65792, 196864));
+    try {
+      await dese.sendShip(65792, 196864, user);
+      assert.fail('should have thrown before');
+    } catch(err) {
+      assertJump(err);
+    }
+  });
+});
