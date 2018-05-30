4f1b03745bebf1f873a752ec31d4345d84209076
Fang <fang@fang.io>
2018-05-31 01:53:50 +0200

    Include solidity-coverage for generating test coverage reports. Run with `./node_modules/.bin/solidity-coverage`.

---
+++ .solcover.js
@@ -0,0 +1,8 @@
+module.exports = {
+    copyPackages: ['zeppelin-solidity'],
+    skipFiles: [ 'interfaces/',
+                 'ERC165Mapping.sol',
+                 'ENSRegistry.sol',
+                 'PublicResolver.sol',
+                 'SafeMath8.sol' ]
+};
