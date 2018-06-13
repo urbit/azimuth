41d715a6527b1b032fea73369e4e0a54318bdc10
Fang <fang@fang.io>
2018-06-13 19:49:55 +0200

    Increase the Truffle gas limit so the Constitution can be deployed. Deployment costs only barely over five million, which is still well under the current livenet limit of approx. eight million.

--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      gas: 5000000,
+      gas: 6000000,
       network_id: "*" // Match any network id
     }
   },
