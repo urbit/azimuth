f3014e99a05ed571cb19fd28555e1632ab14192b
Fang <fang@fang.io>
2018-06-14 17:12:52 +0200

    Include tests helper we've been using for some time...

---
+++ test/helpers/increaseTime.js
@@ -0,0 +1,22 @@
+module.exports = (duration) => {
+  const id = Date.now();
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1);
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id + 1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res);
+      });
+    });
+  });
+};
