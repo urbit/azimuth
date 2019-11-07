96769fc7f53619d824b241a0e950d652a195a473
Fang <git@fang.io>
2019-11-07 16:09:48 +0100

    tmp star release deploy code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,44 +4,37 @@ var Claims = artifacts.require("./Claims.sol");
 var Censures = artifacts.require("./Censures.sol");
 var Ecliptic = artifacts.require("./Ecliptic.sol");
 var DelegatedSending = artifacts.require("./DelegatedSending.sol");
+var LinearSR = artifacts.require("./LinearStarRelease.sol");
+var ConditionalSR = artifacts.require("./ConditionalStarRelease.sol");
 
 const WITH_TEST_STATE = (process.argv[3] === 'with-state');
 
 module.exports = async function(deployer) {
   await deployer;
 
-  // setup contracts
-  const azimuth = await deployer.deploy(Azimuth);
-  const polls = await deployer.deploy(Polls, 1209600, 604800);
-  const claims = await deployer.deploy(Claims, azimuth.address);
-  const censures = await deployer.deploy(Censures, azimuth.address);
-  //NOTE  for real deployment, use a real ENS registry
-  const ecliptic = await deployer.deploy(
-    Ecliptic,
-    '0x0000000000000000000000000000000000000000',
-    azimuth.address, polls.address, claims.address
+  const ecliptic = await Ecliptic.at('0x8b9f86a28921d9c705b3113a755fb979e1bd1bce');
+
+  const csr = await deployer.deploy(ConditionalSR, '0x308ab6a6024cf198b57e008d0ac9ad0219886579',
+    ['0x0000000000000000000000000000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000000000000000000000000001', '0x0000000000000000000000000000000000000000000000000000000000000002'],
+    [1573385696, 1575977696, 1578656096],
+    [1574249696, 1576841696, 1579520096],
+    1582198496
   );
+  const lsr = await deployer.deploy(LinearSR, '0x308ab6a6024cf198b57e008d0ac9ad0219886579');
 
-  // configure contract ownership
-  await azimuth.transferOwnership(ecliptic.address);
-  await polls.transferOwnership(ecliptic.address);
+  const star = (n) => (197 + (256 * n));
 
-  // deploy secondary contracts
-  const sending = await deployer.deploy(DelegatedSending, azimuth.address);
+  // 10 stars, one per day
+  await lsr.register('0x6DEfFb0caFDB11D175F123F6891AA64F01c24F7d', 0, 10, 1, 86400);
+  await ecliptic.setSpawnProxy(star(0), lsr.address);
+  for (let i = 1; i <= 10; i++) {
+    await lsr.deposit('0x6DEfFb0caFDB11D175F123F6891AA64F01c24F7d', star(i));
+  }
+  lsr.startReleasing();
 
-  // beyond this point: "default" state for qa & testing purposes
-  if (!WITH_TEST_STATE) return;
-
-  const own = await ecliptic.owner();
-  await ecliptic.createGalaxy(0, own);
-  await ecliptic.configureKeys(0, '0x123', '0x456', 1, false);
-  await ecliptic.spawn(256, own);
-  await ecliptic.configureKeys(256, '0x456', '0x789', 1, false);
-  // set transfer proxy to delegated sending, very brittle
-  await ecliptic.setSpawnProxy(256, sending.address);
-  await ecliptic.spawn(65792, own);
-  await ecliptic.spawn(131328, own);
-  await ecliptic.spawn(512, own);
-  await sending.setPoolSize(256, 65792, 1000);
-  await ecliptic.createGalaxy(1, own);
+  await csr.register('0x6DEfFb0caFDB11D175F123F6891AA64F01c24F7d', [3, 3, 3], 1, 86400);
+  await ecliptic.setSpawnProxy(star(0), csr.address);
+  for (let i = 11; i <= 19; i++) {
+    await csr.deposit('0x6DEfFb0caFDB11D175F123F6891AA64F01c24F7d', star(i));
+  }
 };
--- truffle.js
+++ truffle.js
@@ -1,14 +1,17 @@
 // Allows us to use ES6 in migrations and tests.
 require('babel-register')
 require('babel-polyfill')
+var PrivateKeyProvider = require("truffle-privatekey-provider");
 
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      gas: 6000000,
-      network_id: "*" // Match any network id
+      provider: function() {
+        return new PrivateKeyProvider('886e44e4e1d22cc0dcd596f59d64cd8744b46a684a2d17c0cbac404641b88537', "http://35.247.55.112:8545");
+      },
+      gas: 3000000,
+      gasPrice: 20000000000,
+      network_id: 3 // Match any network id
     }
   },
   compilers: {
