137038ae9e38d7e86ebf73cbacb0578412ee97f3
Jared Tobin <jared@jtobin.io>
2020-07-16 13:08:37 +0400

    Add missing 'public' visibility attributes.

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -13,11 +13,11 @@ contract Migrations {
     owner = msg.sender;
   }
 
-  function setCompleted(uint completed) restricted {
+  function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) restricted {
+  function upgrade(address new_address) public restricted {
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
   }
